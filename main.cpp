84864303c1aceb121f3c3bef6e5d9280199a4401
백종열 <geeksbaek@gmail.com>
2016-04-12 11:15:32 +0900

    report

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <libnet.h>
+#include <netinet/ether.h>
+#include <arpa/inet.h>
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-void ethernet(u_char *packet);
-void ip(const u_char *packet);
-void tcp(const u_char *packet);
-void udp(const u_char *packet);
-
-int ether_hdr = 0;
-int ip_hdr = ether_hdr+14;
-int tcp_hdr = ip_hdr+20;
-int udp_hdr = ip_hdr+20;
 
 int main()
 {
     pcap_t *handle;
     char *dev = "wlan1";
     char errbuf[PCAP_ERRBUF_SIZE];
-    bpf_u_int32 mask;
-    bpf_u_int32 net;
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-        net = 0;
-        mask = 0;
-    }
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
@@ -34,65 +21,32 @@ int main()
     return(0);
 }
 
-void ethernet(const u_char *packet)
-{
-    int sm, dm;
-    printf("===================ethernet====================\n");
-    printf("\ndestination mac -> ");
-    for(dm=ether_hdr; dm<=ether_hdr+5; dm++) {
-        printf("%02x",packet[dm]);
-        if(dm!=ether_hdr+5)
-            printf(":");
-    }
-    printf("\n\nsource mac -> ");
-    for(sm=ether_hdr+6; sm<=ether_hdr+11; sm++) {
-          printf("%02x",packet[sm]);
-          if(sm!=ether_hdr+11)
-              printf(":");
-    }
-    printf("\n\n");
-}
-
-void ip(const u_char *packet)
-{
-    int si, di;
-    printf("=======================IP=======================\n");
-    printf("\nsource IP -> ");
-    for(si=ip_hdr+12; si<=ip_hdr+15; si++) {
-          printf("%02d",packet[si]);
-          if(si!=ip_hdr+15)
-               printf(".");
-    }
-    printf("\n\ndestination IP -> ");
-    for(di=ip_hdr+16; di<=ip_hdr+19; di++) {
-          printf("%02d",packet[di]);
-          if(di!=ip_hdr+19)
-               printf(".");
-    }
-    printf("\n\n");
-}
-
-void tcp(const u_char *packet)
-{
-    printf("======================TCP=======================\n");
-    printf("\nsource port -> %d", (packet[tcp_hdr] << 8) + packet[tcp_hdr+1]);
-    printf("\n\ndestination port -> %d\n\n", (packet[tcp_hdr+2] << 8) + packet[tcp_hdr+3]);
-}
-
-void udp(const u_char *packet)
-{
-    printf("======================UDP======================\n");
-    printf("\nsource port -> %d", (packet[udp_hdr] << 8) + packet[udp_hdr+1]);
-    printf("\n\ndestination port -> %d\n\n", (packet[udp_hdr] << 8) + packet[udp_hdr+1]);
-}
-
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-    ethernet(packet);
-    if(packet[ether_hdr+12]==8 && packet[ether_hdr+13]==0)
-        ip(packet);
-    if(packet[ip_hdr+9]==6)
-        tcp(packet);
-    if (packet[ip_hdr+9]==17)
-        udp(packet);
+    char buf[24];
+    libnet_ethernet_hdr *eth_header = (libnet_ethernet_hdr*) packet;
+    printf("===================ethernet=====================\n");
+    printf("Destination mac -> %s\n", ether_ntoa_r((ether_addr*)eth_header->ether_dhost, buf));
+    printf("Source mac -> %s\n", ether_ntoa_r((ether_addr*)eth_header-> ether_shost, buf));
+    if(ntohs(eth_header->ether_type)==ETHERTYPE_IP)
+    {
+        libnet_ipv4_hdr *ip_header = (libnet_ipv4_hdr*) (packet + sizeof(libnet_ethernet_hdr));
+        printf("=======================IP=======================\n");
+        printf("Destination IP -> %s\n", inet_ntoa(ip_header->ip_src));
+        printf("Source IP -> %s\n", inet_ntoa(ip_header->ip_dst));
+        if((ip_header->ip_p)==IPPROTO_TCP)
+        {
+            libnet_tcp_hdr *tcp_header = (libnet_tcp_hdr*) (packet + sizeof(libnet_ethernet_hdr) + (ip_header->ip_hl * 4));
+            printf("======================TCP=======================\n");
+            printf("Destination protocol -> %d\n", (int)ntohs(tcp_header->th_dport));
+            printf("Source protocol -> %d\n", (int)ntohs(tcp_header->th_sport));
+        }
+        if((ip_header->ip_p)==IPPROTO_UDP)
+        {
+            libnet_udp_hdr *udp_header = (libnet_udp_hdr*) (packet + sizeof(libnet_ethernet_hdr) + (ip_header->ip_hl * 4));
+            printf("======================UDP=======================\n");
+            printf("Destination protocol -> %d\n", (int)ntohs(udp_header->uh_dport));
+            printf("Source protocol -> %d\n", (int)ntohs(udp_header->uh_sport));
+        }
+    }
 }
